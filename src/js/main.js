538dcfd2a22846c214825889af755566059080fe
Jackson Kaufman <jacksonkaufman115@gmail.com>
2017-07-26 21:27:17 -0600

    Completed RandomChan JS Function

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,26 @@ $(document).ready(function(){
     'show': true,
     'backdrop': false
   });
-
+  $('button.random-chan').click(function(){
+    RandomChan();
+  })
 
 });
+
+function RandomChan(){
+  chanSelection = getRandomIntInclusive(0,chanImages.length - 1);
+  $('#jackiechan .modal-body img').attr('src',chanImages[chanSelection]);
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
+var chanImages = [
+  'images/jackie-chan/jackie-1.jpg',
+  'images/jackie-chan/jackie-2.jpg',
+  'images/jackie-chan/jackie-3.jpg',
+  'images/jackie-chan/jackie-4.jpg',
+]
